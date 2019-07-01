08a916e6f7aad3b6c093bdb37deaab905c5a54de
Mequam <blue9ja@gmail.com>
2019-06-30 20:15:22 -0400

    made it so you can add more than one vote to the polls and reversed the sorting on the votes array
    major re-writes comming in the future, plan is to write a gui system along side the bot

--- bot.py
+++ bot.py
@@ -9,10 +9,8 @@ def sortDict(dicti):
 	#it is designed to be used with the array of emojis inside of the poll class
 	ret_arr = []
 	for word in dicti:
-		ret_arr.append((dicti[word],word))
-	print(ret_arr)
-	ret_arr.sort()
-	print(ret_arr)
+		ret_arr.append((dicti[word],word))	
+	ret_arr.sort(reverse=True)
 	return ret_arr
 class poll:
 	#this is a global array that is shared by each poll object
@@ -24,8 +22,7 @@ class poll:
 		
 		#options is a list of tuples where the first tupple is the text you want to vote for
 		#and the second is the reaction emoji that you use
-		self.options = options
-		print(self.options)
+		self.options = options	
 		self.votes = {}
 		for option in options:
 			#initilize the voting options
@@ -33,9 +30,7 @@ class poll:
 		
 		self.voters = []
 		self.polls.append(self)
-		print(options)
 	def toStr(self):
-		print(self.name)
 		ret_val = self.prompt + '\n'
 		ret_val += '---------------------' + '\n\n\t\t'
 		split_q = self.question.split('\n')
@@ -45,15 +40,19 @@ class poll:
 		ret_val += '\n---------------------\nvotes: '
 		for tup in tList:
 			ret_val += tup[1] + ':' + str(tup[0]) + ' '
-		print(ret_val)
 		return ret_val
 	def addVote(self,emoji,author_id):
 		#this function takes an emoji and mapps it to the votes
+		print('[*] running the voting function\n' + '-'*10)
 		if author_id in self.voters:
-			return False
+			print(str(author_id) + ' has already voted')	
+			return False	
 		elif emoji in self.votes:
+			print('[*] voting as ' + str(author_id))
 			self.votes[emoji] += 1
+			print('[*] ' + str(self.votes))
 			self.voters.append(author_id)
+			return True
 		else:
 			return False  
 	def getPollName(name):
@@ -77,8 +76,11 @@ async def addPoll(ctx,*args):
 		split_a = arg.split(':')
 		if len(split_a) > 1:
 			fields.append((split_a[0],split_a[1]))
-	poll(args[0],args[1],fields)
-	await ctx.send('added poll!')
+	p = poll(args[0],args[1],fields)
+	msg = await ctx.send(p.toStr())
+	for react in p.options:
+		await msg.add_reaction(react[1])
+	#await ctx.send('added poll!')
 @bot.command()
 async def listPolls(ctx,*args):
 	resp = ''
@@ -108,7 +110,6 @@ async def summon(ctx,*args):
 	#remove the last space
 	sent = sent[:-1]
 	p = poll.getPollName(sent)
-	print(p)
 	msg = await ctx.send(p.toStr())
 	for react in p.options:
 		await msg.add_reaction(react[1])
@@ -117,29 +118,27 @@ async def on_ready():
 	print('[*] connected to discord!')
 @bot.event
 async def on_reaction_add(reaction, user):	
+	print('COUNT ' + str(reaction.count))
 	if reaction.message.author.id == clientId: 
 		print('[*] I made that message!')
 		
 		#this is the syntax that this if statement is looking for
 		#poll-<name>:<poll description>	
-		
-		split_m = reaction.message.content.split(':')
-		if len(split_m[0]) > 5 and len(split_m) > 1 and split_m[0].split('-')[0] == 'poll':
-			#we have a valid poll
-			p = poll.getPollName(split_m[0].split('-')[1])
-			p.addVote(reaction.emoji,reaction.message.author.id)
-			reaction.message.clear_reactions()
-			#update the poll
-			#TODO: need to update every message in the cache that changes so all polls are up to date
-			
-			async for user in reaction.users():
-				#this for loop should only fire once if we do our job correctly
-				if user.id != clientId:
+		if user.id != clientId:	
+			split_m = reaction.message.content.split(':')
+			if len(split_m[0]) > 5 and len(split_m) > 1 and split_m[0].split('-')[0] == 'poll':
+				#we have a valid poll
+				p = poll.getPollName(split_m[0].split('-')[1])
+				p.addVote(reaction.emoji,user.id)
+				
+				#update the poll
+				#TODO: need to update every message in the cache that changes so all polls are up to date
+				if reaction.count > 1:
 					await reaction.message.remove_reaction(reaction.emoji,user)
-			#TODO: add a reaciton removal system await reaction.message.remove_reaction(reaction.emoji,'a')
-			await reaction.message.edit(content=p.toStr())
-		print(p.votes)
-		print(reaction.message.content)
+				#TODO: add a reaciton removal system await reaction.message.remove_reaction(reaction.emoji,'a')
+				await reaction.message.edit(content=p.toStr())
+			#print(p.votes)
+			#print(reaction.message.content)
 			
 
 bot.run(get_token())
